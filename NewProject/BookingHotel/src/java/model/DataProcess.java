3b5d5fe330afd752f55fdf2bec6fee134f31ebe1
maitv <tranmai.it7@gmail.com>
2016-04-05 20:47:23 +0700

    fix bug search room available

--- NewProject/BookingHotel/src/java/model/DataProcess.java
+++ NewProject/BookingHotel/src/java/model/DataProcess.java
@@ -284,10 +284,8 @@ public class DataProcess {
         }
 
         String query = "SELECT * FROM Room WHERE roomTypeId=? AND roomStatus != 1 AND roomNumber NOT IN ("
-                //                + " SELECT roomNumber FROM Booking WHERE ? >= checkinDate AND ? <= checkoutDate " + ")";
-                // + " SELECT roomNumber FROM Booking WHERE ( ? BETWEEN checkinDate AND checkoutDate ) OR ( ? BETWEEN checkinDate AND checkoutDate ) " + ")";
                 + "SELECT BookingRoom.roomNumber FROM BookingRoom INNER JOIN Booking ON BookingRoom.bookingId=Booking.bookingId"
-                + " WHERE ( ? BETWEEN Booking.checkinDate AND Booking.checkoutDate ) OR ( ? BETWEEN Booking.checkinDate AND Booking.checkoutDate ))";
+                + " WHERE (Booking.status!=0 AND Booking.status!=1 AND Booking.status!=2) AND (( ? BETWEEN Booking.checkinDate AND Booking.checkoutDate ) OR ( ? BETWEEN Booking.checkinDate AND Booking.checkoutDate )))";
         try {
             PreparedStatement prst = cnn.prepareCall(query);
 
